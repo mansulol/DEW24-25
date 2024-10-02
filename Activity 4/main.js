ecbc75bbd56aadd37d0488664528b4a31a7ea982
Mansour <mansourlol440@gmail.com>
2024-10-02 20:32:34 +0100

    Actividad 4

--- Activity 4/main.js
+++ Activity 4/main.js
@@ -4,3 +4,26 @@ var employees = [
     {"id":3,"name":"FRANCISCO JOSE GRAÑA BARBERA","department":"IT"},
     {"id":4,"name":"IRENE POMBO JORDA","department":"HR"}
 ];
+
+// employees.forEach((employe) => {
+//     alert(
+//     `Id: ${employe.id}, 
+//     Name: ${employe.name}, 
+//     Departament: ${employe.department}`)
+// })
+
+let container = document.getElementById('tbody')
+
+employees.forEach((employe) => {
+    let row = document.createElement('tr')
+
+    let id = document.createElement('td').innerHTML="SI"
+    let name = document.createElement('td').innerHTML="No"
+    let department = document.createElement('td').innerHTML="No se"
+
+    row.append( id )
+    row.append(name)
+    row.append(department)
+
+    container.append( row )
+})
